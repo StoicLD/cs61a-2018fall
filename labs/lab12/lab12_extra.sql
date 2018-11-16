a014b01939844fa241583fd7231db9d22797f735
Stoic <729790146@qq.com>
2018-11-15 21:49:13 -0800

    finish lab 12

--- labs/lab12/lab12_extra.sql
+++ labs/lab12/lab12_extra.sql
@@ -2,20 +2,29 @@
 
 -- Q5
 CREATE TABLE fa18favpets AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT pet, count(pet)
+  from students
+  group by pet
+  order by count(pet) DESC
+  limit 10;
 
 
 CREATE TABLE fa18dog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT * from fa18favpets where pet = "dog";
 
 
 CREATE TABLE fa18alldogs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT pet, count(pet) from students where pet like "%dog%";
 
 
 CREATE TABLE obedienceimages AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT seven, denero ,count(*)
+  from students
+  where seven = '7'
+  group by denero;
 
 -- Q6
-CREATE TABLE smallest_int_count AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+CREATE TABLE smallest_int_count AS 
+  select smallest, count(*)
+  from students
+  group by smallest;
